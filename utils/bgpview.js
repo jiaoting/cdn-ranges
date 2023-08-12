ad4b0678702a2698acd4293e1f7573f4ad474386
ᄃΉЯIƧƬӨPΉΣЯ (BᄂΣᄃKMΛПП-)DЯΣΉΣЯ <root@schniggie.de>
2023-08-12 15:13:17 +0200

    Update bgpview.js
    Code modified to support multiple ASNs:
    - bgpView module now accepts an array of ASNs instead of a single asn parameter
    - added delay of 3 seconds between calls to prevent HTTP 429

--- utils/bgpview.js
+++ utils/bgpview.js
@@ -1,10 +1,19 @@
-const rax = require('retry-axios')
-const axios = require('axios')
+const rax = require('retry-axios');
+const axios = require('axios');
 
 const interceptorId = rax.attach();
 
-module.exports = async (asn) => {
-  const { data } = await axios.get(`https://api.bgpview.io/asn/${asn}/prefixes`)
+module.exports = async (asns) => {
+  const prefixes = [];
+  
+  for (const asn of asns) {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 3000); // Delay for 3 seconds
+    });
 
-  return data.data.ipv4_prefixes.map(({ prefix }) => prefix)
-}
+    const { data } = await axios.get(`https://api.bgpview.io/asn/${asn}/prefixes`);
+    prefixes.push(...data.data.ipv4_prefixes.map(({ prefix }) => prefix));
+  }
+
+  return prefixes;
+};
